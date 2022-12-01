3ece3c9f89783bb63862757a7c6e33574aa87a94
JIM <jimmyx27@gmail.com>
2022-12-01 17:54:52 -0500

    Fix typo from pull

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,7 +10,7 @@ const Billing = () => (
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"></div>
       </div>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Easilllly control your <br className="sm:block hidden" /> billing & invoicing</h2>
+        <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" /> billing & invoicing</h2>
         <p className={`${styles.paragraph} max-w-[470px]mt-5`}>
             Elit enim sed massa etiam.
           </p>
